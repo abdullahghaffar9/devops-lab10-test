041a8b8b25089d25564dfb66c474aac86f329b49
abdullahghaffar9 <abdullahghaffar2003@gmail.com>
2026-04-25 07:56:48 +0000

    Fix: Re-pasted clean Java test code

--- src/test/java/lab/LoginTest.java
+++ src/test/java/lab/LoginTest.java
@@ -1,4 +1,3 @@
-// Triggering Jenkins Pipeline Test
 package lab;
 
 import org.junit.jupiter.api.Test;
@@ -13,29 +12,26 @@ public class LoginTest {
 
     @Test
     void test_login_with_incorrect_credentials() {
-        // We REMOVED the Windows System.setProperty line here.
-        // The Docker image already has ChromeDriver in its system path.
-        
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");               // Run in headless mode [cite: 9]
-        options.addArguments("--no-sandbox");             // Bypass OS security model (needed for Docker)
-        options.addArguments("--disable-dev-shm-usage");  // Overcome limited resource problems in Docker
-        options.addArguments("--remote-allow-origins=*"); // Prevent connection reset issues
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--remote-allow-origins=*");
 
         WebDriver driver = new ChromeDriver(options);
         
         try {
-            driver.navigate().to("http://103.139.122.250/"); [cite: 11]
-            driver.findElement(By.name("email")).sendKeys("qasim@malik.com"); [cite: 12]
-            driver.findElement(By.name("password")).sendKeys("abcdefg"); [cite: 13]
-            driver.findElement(By.id("m_login_signin_submit")).click(); [cite: 14]
+            driver.navigate().to("http://103.139.122.250/");
+            driver.findElement(By.name("email")).sendKeys("qasim@malik.com");
+            driver.findElement(By.name("password")).sendKeys("abcdefg");
+            driver.findElement(By.id("m_login_signin_submit")).click();
             
-            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS); [cite: 15]
+            driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
             
-            String errorText = driver.findElement(By.xpath("/html/body/div/div/div[1]/div/div/div/div[2]/form/div[1]")).getText(); [cite: 16]
-            assertTrue(errorText.contains("Incorrect email or password")); [cite: 17]
+            String errorText = driver.findElement(By.xpath("/html/body/div/div/div[1]/div/div/div/div[2]/form/div[1]")).getText();
+            assertTrue(errorText.contains("Incorrect email or password"));
         } finally {
-            driver.quit(); [cite: 18]
+            driver.quit();
         }
     }
 }
